3f2eb14ad151fddfcbe60770359f9ffb82faf97f
Максим Федосеев <max.faceless.frei@gmail.com>
2017-02-03 12:03:18 +0500

    all: cleanup and update vendored dependencies
    Remove github.com/olekukonko/ts, since it's not used by
    github.com/cheggaaa/pb anymore and update all other dependencies.

--- vendor/github.com/valyala/fasthttp/server.go
+++ vendor/github.com/valyala/fasthttp/server.go
@@ -1480,8 +1480,8 @@ func (s *Server) serveConn(c net.Conn) error {
 			}
 		} else {
 			br, err = acquireByteReader(&ctx)
-			ctx.Request.isTLS = isTLS
 		}
+		ctx.Request.isTLS = isTLS
 
 		if err == nil {
 			if s.DisableHeaderNamesNormalizing {
