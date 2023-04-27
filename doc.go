4a9acba818667d43a1e04bc2b75e6a91d0257787
Maksim (codesenberg) Fedoseev <max.faceless.frei@gmail.com>
2023-04-27 06:47:48 +0100

    bombardier: minor formatting adjustments

--- doc.go
+++ doc.go
@@ -3,61 +3,66 @@ Command line utility bombardier is a fast cross-platform HTTP
 benchmarking tool written in Go.
 
 Installation with Go 1.17+:
-  go install github.com/codesenberg/bombardier@latest
+
+	go install github.com/codesenberg/bombardier@latest
 
 Installation with older versions of Go:
-  go get -u github.com/codesenberg/bombardier
+
+	go get -u github.com/codesenberg/bombardier
 
 Usage:
-  bombardier [<flags>] <url>
+
+	bombardier [<flags>] <url>
 
 Flags:
-      --help                  Show context-sensitive help (also try --help-long
-                              and --help-man).
-      --version               Show application version.
-  -c, --connections=125       Maximum number of concurrent connections
-  -t, --timeout=2s            Socket/request timeout
-  -l, --latencies             Print latency statistics
-  -m, --method=GET            Request method
-  -b, --body=""               Request body
-  -f, --body-file=""          File to use as request body
-  -s, --stream                Specify whether to stream body using chunked
-                              transfer encoding or to serve it from memory
-      --cert=""               Path to the client's TLS Certificate
-      --key=""                Path to the client's TLS Certificate Private Key
-  -k, --insecure              Controls whether a client verifies the server's
-                              certificate chain and host name
-  -H, --header="K: V" ...     HTTP headers to use(can be repeated)
-  -n, --requests=[pos. int.]  Number of requests
-  -d, --duration=10s          Duration of test
-  -r, --rate=[pos. int.]      Rate limit in requests per second
-      --fasthttp              Use fasthttp client
-      --http1                 Use net/http client with forced HTTP/1.x
-      --http2                 Use net/http client with enabled HTTP/2.0
-  -p, --print=<spec>          Specifies what to output. Comma-separated list of
-                              values 'intro' (short: 'i'), 'progress' (short:
-                              'p'), 'result' (short: 'r'). Examples:
 
-                                * i,p,r (prints everything)
-                                * intro,result (intro & result)
-                                * r (result only)
-                                * result (same as above)
-  -q, --no-print              Don't output anything
-  -o, --format=<spec>         Which format to use to output the result. <spec>
-                              is either a name (or its shorthand) of some format
-                              understood by bombardier or a path to the
-                              user-defined template, which uses Go's
-                              text/template syntax, prefixed with 'path:' string
-                              (without single quotes), i.e.
-                              "path:/some/path/to/your.template" or
-                              "path:C:\some\path\to\your.template" in case of
-                              Windows. Formats understood by bombardier are:
+	    --help                  Show context-sensitive help (also try --help-long
+	                            and --help-man).
+	    --version               Show application version.
+	-c, --connections=125       Maximum number of concurrent connections
+	-t, --timeout=2s            Socket/request timeout
+	-l, --latencies             Print latency statistics
+	-m, --method=GET            Request method
+	-b, --body=""               Request body
+	-f, --body-file=""          File to use as request body
+	-s, --stream                Specify whether to stream body using chunked
+	                            transfer encoding or to serve it from memory
+	    --cert=""               Path to the client's TLS Certificate
+	    --key=""                Path to the client's TLS Certificate Private Key
+	-k, --insecure              Controls whether a client verifies the server's
+	                            certificate chain and host name
+	-H, --header="K: V" ...     HTTP headers to use(can be repeated)
+	-n, --requests=[pos. int.]  Number of requests
+	-d, --duration=10s          Duration of test
+	-r, --rate=[pos. int.]      Rate limit in requests per second
+	    --fasthttp              Use fasthttp client
+	    --http1                 Use net/http client with forced HTTP/1.x
+	    --http2                 Use net/http client with enabled HTTP/2.0
+	-p, --print=<spec>          Specifies what to output. Comma-separated list of
+	                            values 'intro' (short: 'i'), 'progress' (short:
+	                            'p'), 'result' (short: 'r'). Examples:
 
-                                * plain-text (short: pt)
-                                * json (short: j)
+	                              * i,p,r (prints everything)
+	                              * intro,result (intro & result)
+	                              * r (result only)
+	                              * result (same as above)
+	-q, --no-print              Don't output anything
+	-o, --format=<spec>         Which format to use to output the result. <spec>
+	                            is either a name (or its shorthand) of some format
+	                            understood by bombardier or a path to the
+	                            user-defined template, which uses Go's
+	                            text/template syntax, prefixed with 'path:' string
+	                            (without single quotes), i.e.
+	                            "path:/some/path/to/your.template" or
+	                            "path:C:\some\path\to\your.template" in case of
+	                            Windows. Formats understood by bombardier are:
+
+	                              * plain-text (short: pt)
+	                              * json (short: j)
 
 Args:
-  <url>  Target's URL
+
+	<url>  Target's URL
 
 For detailed documentation on user-defined templates see
 documentation for package github.com/codesenberg/bombardier/template.
--- template/doc.go
+++ template/doc.go
@@ -6,36 +6,36 @@ User-defined templates use Go's text/template package, so you might
 want to check its documentation first.
 There are a bunch of helper methods available inside a template
 besides those described in aforementioned documentation, namely:
-	- WithLatencies()
-		Tells whether --latencies flag were activated.
-	- FormatBinary(numberOfBytes float64) string
-		Converts bytes to kilo-, mega-, giga-, etc.- bytes, and
-		appends appropriate suffix "KB", "MB", "GB", etc.
-	- FormatTimeUs(us float64) string
-		Converts microseconds to milliseconds, seconds, minutes or
-		hours and appends appropriate suffix.
-	- FormatTimeUsUint64(us uint64) string
-		Same as above, but for uint64, since type conversions are
-		not available in templates.
-	- FloatsToArray(ps ...float64) []float64
-		Converts a bunch of floats into array, since, again,
-		type conversions are not available in templates.
-	- Multiply(num, coeff float64) float64
-		Arithmetics are not available inside of templates either.
-	- StringToBytes(s string) []byte
-		Convenience function to convert string to []byte.
-	- UUIDV1() (UUID, error)
-		Generates UUID Version 1, based on timestamp and
-		MAC address (RFC 4122)
-	- UUIDV2(domain byte) (UUID, error)
-		Generates UUID Version 2, based on timestamp, MAC address
-		and POSIX UID/GID (DCE 1.1)
-	- UUIDV3(ns UUID, name string) UUID
-		Generates UUID Version 3, based on MD5 hashing (RFC 4122)
-	- UUIDV4() (UUID, error)
-		Generates UUID Version 4, based on random numbers (RFC 4122)
-	- UUIDV5(ns UUID, name string) UUID
-		Generates UUID Version 5, based on SHA-1 hashing (RFC 4122)
+  - WithLatencies()
+    Tells whether --latencies flag were activated.
+  - FormatBinary(numberOfBytes float64) string
+    Converts bytes to kilo-, mega-, giga-, etc.- bytes, and
+    appends appropriate suffix "KB", "MB", "GB", etc.
+  - FormatTimeUs(us float64) string
+    Converts microseconds to milliseconds, seconds, minutes or
+    hours and appends appropriate suffix.
+  - FormatTimeUsUint64(us uint64) string
+    Same as above, but for uint64, since type conversions are
+    not available in templates.
+  - FloatsToArray(ps ...float64) []float64
+    Converts a bunch of floats into array, since, again,
+    type conversions are not available in templates.
+  - Multiply(num, coeff float64) float64
+    Arithmetics are not available inside of templates either.
+  - StringToBytes(s string) []byte
+    Convenience function to convert string to []byte.
+  - UUIDV1() (UUID, error)
+    Generates UUID Version 1, based on timestamp and
+    MAC address (RFC 4122)
+  - UUIDV2(domain byte) (UUID, error)
+    Generates UUID Version 2, based on timestamp, MAC address
+    and POSIX UID/GID (DCE 1.1)
+  - UUIDV3(ns UUID, name string) UUID
+    Generates UUID Version 3, based on MD5 hashing (RFC 4122)
+  - UUIDV4() (UUID, error)
+    Generates UUID Version 4, based on random numbers (RFC 4122)
+  - UUIDV5(ns UUID, name string) UUID
+    Generates UUID Version 5, based on SHA-1 hashing (RFC 4122)
 
 The structure that gets passed to the template is documented in
 the package github.com/codesenberg/bombardier/internal. The structure
